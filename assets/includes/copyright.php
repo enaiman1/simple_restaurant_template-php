095fe31f3a8aaac679383de80d440ad00e93b26e
Ericcentrik <enaiman1@gmail.com>
2021-01-29 13:17:15 -0500

    added basic skelton and style

---
+++ assets/includes/copyright.php
@@ -0,0 +1,2 @@
+<h4>Simple Restaurant Template</h4>
+<h5>Copyright &copy;<?php echo date('Y');?> <a href="https://ericnaiman.com/" target="_blank">Eric Naiman</a>
---
+++ index.php
@@ -0,0 +1,67 @@
+<html> 
+    <head>
+        <title></title>
+        <link href="/assets/styles.css" rel="stylesheet">
+    </head>
+
+    <body id="final-example">
+        <div class="wrapper">
+
+            <div id="banner">
+                <a href="/" title="Return to Home">
+                    <img src="./assets/img/banner.png" alt="Franklins Fine Dinning logo">
+                </a>
+            </div> <!--end banner -->
+
+            <div id="nav">
+                <ul>
+                    <li> <a href="index.php">Home</a> </li>
+                    <li> <a href="team.php">Team</a> </li>
+                    <li> <a href="menu.php">Menu</a> </li>
+                    <li> <a href="contact.php">Contact</a> </li>
+                </ul>
+            </div> <!--end nav -->
+
+            <div class="content">
+
+                <div id="footer" class="cf">
+
+                    <div class="column three">
+                        <strong>Phone</strong>
+                        555.555.4444
+                    </div> <!--end left column -->
+
+                    <div class="column three">
+                        <strong>Location</strong>
+                        888 international drive.<br>
+                        Orlando, FL
+                    </div> <!--end middle column -->
+
+                    <div class="column three last">
+                        <strong>Hours</strong>
+                        <em>Monday - Thursday<em>
+                        11am - 9pm<br><br>
+
+                        <strong>Hours</strong>
+                        <em>Friday - Saturday<em>
+                        11am - 11pm<br><br>
+
+                        <strong>Hours</strong>
+                        <em>Sunday<em>
+                        9am - 9pm<br><br>
+
+                    </div> <!--end right column -->
+
+                </div><!--end footer -->
+
+                <small>&copy; 20201 Eric Naiman <small>
+
+            </div> <!--end content -->
+
+        </div> <!--end wrapper -->
+
+        <div class="copyright-info">
+            <?php include('./assets/includes/copyright.php'); ?>
+        </div> <!--end copy right info -->
+    </body>
+</html>
