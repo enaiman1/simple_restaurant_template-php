24c44a5ac455268c56ef046733976a6cad1207e0
Ericcentrik <enaiman1@gmail.com>
2021-01-30 10:33:02 -0500

    seprate index.php into components, made nav bar dynamic, add sotre-hours pluging

---
+++ assets/includes/footer.php
@@ -0,0 +1,43 @@
+<div id="footer" class="cf">
+
+<div class="column three">
+    <strong>Phone</strong>
+    555.555.4444
+</div> <!--end left column -->
+
+<div class="column three">
+    <strong>Location</strong>
+    888 international drive.<br>
+    Orlando, FL
+</div> <!--end middle column -->
+
+<div class="column three last">
+    <strong>Hours</strong>
+    <em>Monday - Thursday<em>
+    11am - 9pm<br><br>
+
+    <strong>Hours</strong>
+    <em>Friday - Saturday<em>
+    11am - 11pm<br><br>
+
+    <strong>Hours</strong>
+    <em>Sunday<em>
+    9am - 9pm<br><br>
+
+    <?php include('./assets/includes/store-hours.php'); ?>
+
+</div> <!--end right column -->
+
+</div><!--end footer -->
+
+<small>&copy; <?php echo date('Y'); ?> <?php echo $companyName; ?> <small>
+
+</div> <!--end content -->
+
+</div> <!--end wrapper -->
+
+<div class="copyright-info">
+<?php include('./assets/includes/copyright.php'); ?>
+</div> <!--end copy right info -->
+</body>
+</html>
---
+++ assets/includes/header.php
@@ -0,0 +1,26 @@
+<?php
+$companyName = "Franklins Fine Dining";
+include('./assets/includes/arrays.php');
+
+?>
+
+<html> 
+    <head>
+        <title></title>
+        <link href="/assets/styles.css" rel="stylesheet">
+    </head>
+
+    <body id="final-example">
+        <div class="wrapper">
+
+            <div id="banner">
+                <a href="/" title="Return to Home">
+                    <img src="./assets/img/banner.png" alt="Franklins Fine Dinning logo">
+                </a>
+            </div> <!--end banner -->
+
+            <div id="nav">
+                    <?php include('./assets/includes/nav.php'); ?>
+            </div> <!--end nav -->
+
+            <div class="content">
