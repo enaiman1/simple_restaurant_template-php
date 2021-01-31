79387f55b9975b112b3bdbbce359434b645cecce
Ericcentrik <enaiman1@gmail.com>
2021-01-31 10:52:06 -0500

    updated array.php, started building out menu.php

--- assets/includes/arrays.php
+++ assets/includes/arrays.php
@@ -43,6 +43,34 @@ $teamMembers = array (
         "bio" => "Carlos is the epitome of the phrase &ldquo; Don't jude a book by it's cover&rdquo; &mdash; You simply cannot find a better chef",
         "img" => "carlos" 
     ),
-)
+);
+
+//Menu Items
+$menuItems = array(
+    "club-sandwhich" => array(
+        "title" => "Club Sandwhich",
+        "price" => 11,
+        "description" =>  "Bacon ipsum dolor amet tri-tip qui pork belly consequat cupidatat. Swine est landjaeger chislic non labore mollit ham hock, andouille velit alcatra tail meatloaf beef ribs. Eiusmod filet mignon ut ut, alcatra dolore sirloin. Alcatra beef ribs pork belly spare ribs, doner drumstick fugiat non turducken quis.",
+        "drink" => "Club Soda"
+    ),
+    "dill-salmon" => array(
+        "title" => "Lemon &amp; Dill Salmon",
+        "price" => 34,
+        "description" =>  "Crestfish butterfly ray sea dragon ghost pipefish horn shark, Siamese fighting fish Owens pupfish pollock, torrent catfish whale catfish. Splitfin driftfish Redfin perch Bigscale pomfret zander ruffe, horn shark pikeblenny Atlantic trout mora whiff lampfish. ",
+        "drink" => "Fresh Press limeade"
+    ),
+    "super-salad" => array(
+        "title" => "The Super salad",
+        "price" => 18,
+        "description" =>  "Crestfish butterfly ray sea dragon ghost pipefish horn shark, Siamese fighting fish Owens pupfish pollock, torrent catfish whale catfish. Splitfin driftfish Redfin perch Bigscale pomfret zander ruffe, horn shark pikeblenny Atlantic trout mora whiff lampfish. ",
+        "drink" => "Cucumber  &amp; mint infused water"
+    ),
+    "mexican-barbacoa" => array(
+        "title" => "The Super salad",
+        "price" => 23,
+        "description" =>  "Velit dolore sirloin turducken beef in do venison. Tail short ribs tongue sausage salami nulla. Eu tri-tip cow quis, pork chop tenderloin in brisket laboris swine commodo. Sint filet mignon ribeye adipisicing in irure ea consequat consectetur bresaola short loin.",
+        "drink" => "Cadillac Margarita"
+    ),
+);
 
 ?>
---
+++ menu.php
@@ -0,0 +1,21 @@
+<?php
+define("TITLE", "Menu | Franklin's Fine Dinning");
+
+include( './assets/includes/header.php' );
+?>
+
+<div id="menu-items">
+    <h1>Our Delicious Menu</h1>
+    <p>Like our team, our menu is very small &mdash; but dang, does it ever pack a punch!</p>
+    <p><em>Click any menu item to learn more about it.</em></p>
+    <hr>
+    <ul>
+        <?php foreach ($menuItems as $dish => $item) {?>
+            <li><a href="dish.php?item=<?php echo $dish;?>"><?php echo $item["title"];?></a><sup>$</sup><?php echo $item["price"];?></li>
+        <?php } ?>
+    </ul>
+</div> <!--  end menu-items -->
+
+<?php
+include( './assets/includes/footer.php' );
+?>
